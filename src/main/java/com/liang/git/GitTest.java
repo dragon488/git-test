3cd921a5919d8d53ae442fbe25e66d72bd39064b
Liang <2376129140@qq.com>
2022-07-23 15:09:25 +0800

    first commit

---
+++ src/main/java/com/liang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.liang.git;
+
+/**
+ * @author Liang
+ * @create 2022-07-23 15:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
